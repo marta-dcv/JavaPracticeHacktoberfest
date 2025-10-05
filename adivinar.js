d3b2f1d70d4b620999cf54f3ddeb598fb043714b
marta-dcv <marta.decastr9@gmail.com>
2025-10-05 10:08:45 +0200

    cuarto

---
+++ adivinar.js
@@ -0,0 +1,14 @@
+const numeroSecreto = Math.floor(Math.random() * 100) + 1;
+
+function adivinarNumero(intento) {
+    if (intento === numeroSecreto) {
+        return "¡Correcto! 🎉";
+    } else if (intento < numeroSecreto) {
+        return "Demasiado bajo ⬇️";
+    } else {
+        return "Demasiado alto ⬆️";
+    }
+}
+
+
+console.log(adivinarNumero(50));
