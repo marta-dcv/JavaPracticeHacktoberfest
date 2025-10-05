631abe6d21a21baa16a033328320af46e4a00e1c
marta-dcv <marta.decastr9@gmail.com>
2025-10-05 09:54:20 +0200

    subo primer proyecto

---
+++ edad.js
@@ -0,0 +1,14 @@
+function calcularEdad(fechaNacimiento) {
+  const hoy = new Date();
+  const cumple = new Date(fechaNacimiento);
+  let edad = hoy.getFullYear() - cumple.getFullYear();
+  const mes = hoy.getMonth() - cumple.getMonth();
+
+  if (mes < 0 || (mes === 0 && hoy.getDate() < cumple.getDate())) {
+    edad--;
+  }
+
+  return edad;
+}
+
+console.log("Edad:", calcularEdad("2000-10-05"), "años");
