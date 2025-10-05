01c53bdb601303f1e404b6aa5c5ed85f9d70fb9f
marta-dcv <marta.decastr9@gmail.com>
2025-10-05 10:05:58 +0200

    segundo

---
+++ vocales.java
@@ -0,0 +1,19 @@
+public class ContarVocales {
+    public static void main(String[] args) {
+        String texto = "Hacktoberfest es divertido!";
+        int contador = contarVocales(texto);
+        System.out.println("Cantidad de vocales: " + contador);
+    }
+
+    public static int contarVocales(String texto) {
+        int total = 0;
+        texto = texto.toLowerCase();
+        for (int i = 0; i < texto.length(); i++) {
+            char c = texto.charAt(i);
+            if ("aeiouáéíóú".indexOf(c) != -1) {
+                total++;
+            }
+        }
+        return total;
+    }
+}
